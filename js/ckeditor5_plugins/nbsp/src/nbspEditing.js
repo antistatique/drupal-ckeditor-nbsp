b1ec2ab92050694692d6a01f96ae03bede6ea577
Kevin Wenger <wenger.kev@gmail.com>
2022-12-02 16:18:23 +0100

    remove extra space

--- js/ckeditor5_plugins/nbsp/src/nbspEditing.js
+++ js/ckeditor5_plugins/nbsp/src/nbspEditing.js
@@ -19,18 +19,18 @@ export default class NbspEditing extends Plugin {
         conversion.elementToElement({model: 'nbsp', view: 'nbsp'});
     }
 
-
     init() {
-        const editor = this.editor;
-        this.editor.commands.add('nbsp', new NbspCommand(this.editor));
-        this._defineSchema();
-        this._defineConverters();
-        // Insert  if Ctrl+Space is pressed:
-        editor.keystrokes.set(['ctrl', 32], (data, cancel) => {
-            editor.commands.execute('nbsp');
-            // Prevent the default (native) action and stop the underlying keydown
-            // event so no other editor feature will interfere.
-            cancel();
-        });
+      const editor = this.editor;
+      this.editor.commands.add('nbsp', new NbspCommand(this.editor));
+      this._defineSchema();
+      this._defineConverters();
+
+      // Insert if Ctrl+Space is pressed:
+      editor.keystrokes.set(['ctrl', 32], (data, cancel) => {
+          editor.commands.execute('nbsp');
+          // Prevent the default (native) action and stop the underlying keydown
+          // event so no other editor feature will interfere.
+          cancel();
+      });
     }
 }
